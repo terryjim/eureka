0a13b428d3eede63c6d0564f36868e59a4c4d04f
terryjim <terryjim@163.com>
2017-12-05 12:56:11 +0800

    init

---
+++ src/test/java/cn/com/topit/erueka/EruekaApplicationTests.java
@@ -0,0 +1,18 @@
+package cn.com.topit.erueka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import cn.com.topit.eureka.Application;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class EruekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
